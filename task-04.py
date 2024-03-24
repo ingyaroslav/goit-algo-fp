5ac022d1f21362ba568a3a29a8e461a7bc1386ae
ingyaroslav <ingyaroslav@gmail.com>
2024-03-24 14:12:48 +0200

    Task 04

--- task-04.py
+++ task-04.py
@@ -1,69 +1,49 @@
 import uuid
-import networkx as nx  # Додайте цей імпорт
-
+import networkx as nx
 import matplotlib.pyplot as plt
 
-class Node:
+class MinHeapNode:
     def __init__(self, key, color="skyblue"):
         self.left = None
         self.right = None
         self.val = key
-        self.color = color  # Додатковий аргумент для зберігання кольору вузла
-        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+        self.color = color
+        self.id = str(uuid.uuid4())
 
-def add_edges(graph, node, pos, x=0, y=0, layer=1):
+def add_heap_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
-        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
             pos[node.left.id] = (l, y - 1)
-            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+            l = add_heap_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
             graph.add_edge(node.id, node.right.id)
             r = x + 1 / 2 ** layer
             pos[node.right.id] = (r, y - 1)
-            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+            r = add_heap_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
     return graph
 
-def draw_tree(tree_root):
-    tree = nx.DiGraph()
-    pos = {tree_root.id: (0, 0)}
-    tree = add_edges(tree, tree_root, pos)
+def draw_heap(heap_root):
+    heap = nx.DiGraph()
+    pos = {heap_root.id: (0, 0)}
+    heap = add_heap_edges(heap, heap_root, pos)
 
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
-
-    plt.figure(figsize=(8, 5))
-    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
-    plt.show()
-
-def draw_heap(heap):
-    heap_tree = nx.DiGraph()
-    pos = {}
-
-    for i, val in enumerate(heap):
-        node = Node(val)
-        node.id = str(uuid.uuid4())
-        heap_tree.add_node(node.id, label=str(val))
-
-        parent_index = (i - 1) // 2
-        if parent_index >= 0:
-            parent_id = heap_tree.nodes()[parent_index]
-            heap_tree.add_edge(parent_id, node.id)
-
-        if i % 2 == 0:  # right child
-            pos[node.id] = (pos[heap_tree.nodes()[parent_index]][0] + 0.5, -1)
-        else:  # left child
-            pos[node.id] = (pos[heap_tree.nodes()[parent_index]][0] - 0.5, -1)
-
-    colors = ["skyblue"] * len(heap)  # Задаємо всім вузлам колір
-    labels = {node[0]: node[1]['label'] for node in heap_tree.nodes(data=True)}
+    colors = [node[1]['color'] for node in heap.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in heap.nodes(data=True)}
 
     plt.figure(figsize=(8, 5))
-    nx.draw(heap_tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    nx.draw(heap, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
     plt.show()
 
-# Приклад використання
-heap = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-draw_heap(heap)
+# Створення міні-купи
+root_heap = MinHeapNode(0)
+root_heap.left = MinHeapNode(4)
+root_heap.left.left = MinHeapNode(5)
+root_heap.left.right = MinHeapNode(10)
+root_heap.right = MinHeapNode(1)
+root_heap.right.left = MinHeapNode(3)
+
+# Відображення міні-купи
+draw_heap(root_heap)
